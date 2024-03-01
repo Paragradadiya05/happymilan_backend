c3906639fe08de607a04939e81fab3d5f34de791
parag-radadiya <parag.180410107091@gmail.com>
2024-03-01 16:52:12 +0530

    email

--- services/email.service.js
+++ services/email.service.js
@@ -330,8 +330,7 @@ export const sendOtpVerificationEmail = async (user, otp) => {
   const { email: to, name } = user;
   const subject = 'Otp verification email!';
   const text = `
- <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
     <meta charset="utf-8">
@@ -351,16 +350,15 @@ export const sendOtpVerificationEmail = async (user, otp) => {
         }
 
         #logo img {
-            display: block;
-            margin: 0 auto;
+            align-items: center;
+            margin-left: 280px;
             padding-top: 30px;
             height: 24px;
             width: 85px;
         }
 
         #content div {
-            width: 100%;
-            padding: 0 20px;
+            width: 622px;
         }
 
         #content div p {
@@ -373,9 +371,7 @@ export const sendOtpVerificationEmail = async (user, otp) => {
         }
 
         #content {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
+            margin-left: 40px;
             margin-top: 30px;
         }
 
@@ -397,31 +393,32 @@ export const sendOtpVerificationEmail = async (user, otp) => {
         }
 
         #footer {
-            width: 100%;
+            width: 607px;
+            height: 60px;
             border-top: 1px solid #E2E2E2;
             text-align: center;
             padding-top: 10px;
             margin-top: 30px;
+
         }
 
         #footer-content {
-            width: 100%;
-            padding: 0 20px;
+            width: 600px;
         }
+
 
         #footer div {
             display: flex;
-            flex-direction: column;
+            justify-content: space-between;
         }
 
-        #footer div #ul-1,
-        #footer div #ul-2 {
+        #footer div #ul-1 {
             list-style-type: none;
-            padding: 0;
+            position: relative;
+            left: -30px;
         }
 
-        #footer div #ul-1 li span,
-        #footer div #ul-2 li span {
+        #footer div #ul-1 li span {
             color: #000;
             font-family: Poppins;
             font-size: 10px;
@@ -430,55 +427,91 @@ export const sendOtpVerificationEmail = async (user, otp) => {
             line-height: normal;
         }
 
+        #footer div #ul-2 li span {
+            color: #0F52BA;
+            font-family: Poppins;
+            font-size: 10px;
+            font-style: normal;
+            font-weight: 900;
+            line-height: normal;
+        }
+
+
         #footer div #ul-2 li {
+            list-style-type: none;
             display: flex;
             gap: 20px;
         }
-         @media only screen and (max-width: 600px) {
+
+        #footer div #ul-2 {
+            display: flex;
+            gap: 40px;
+            justify-content: space-between;
+        }
+
+        @media only screen and (max-width: 600px) {
             #parrent {
                 margin-left: 0;
             }
+
+            #footer-content {
+                width: 350px;
+            }
+
+            #logo img {
+                align-items: center;
+                margin-left: 270px;
+                padding-top: 30px;
+                height: 24px;
+                width: 85px;
+            }
+
         }
-  
-        
     </style>
 
-       
+
+
 </head>
 
 <body>
-    <div id="center">
-        <div id="parrent">
-            <div id="logo">
-                <img src="https://happymilan-user-images.s3.ap-south-1.amazonaws.com/logoImage/logo.jpg" alt="logo" />
+    <div id="parrent">
+        <div id="logo">
+            <img src="https://happymilan-user-images.s3.ap-south-1.amazonaws.com/logoImage/logo.jpg" alt="logo" />
+        </div>
+        <div id="content">
+            <div>
+                <p>Dear User,${name}</p>
+                <p>Thank you for using our services. To complete the verification process, please enter the
+                    following One-Time Password (OTP) on the verification page:</p>
+                <h1 id="OTP-Text"> ${otp}</h1> <!--OTP Variable -->
+                <p>Please do not share this OTP with anyone for security reasons</p>
             </div>
-            <div id="content">
-                <div>
-                    <p>Dear User,${name}</p>
-                    <p>Thank you for using our services. To complete the verification process, please enter the
-                        following One-Time Password (OTP) on the verification page:</p>
-                    <h1 id="OTP-Text"> OTP:${otp}</h1>
-                    <!--OTP Variable -->
-                    <p>Please do not share this OTP with anyone for security reasons</p>
-                </div>
-            </div>
-            <div id="footer-box">
-                <div id="footer">
-                    <div id="footer-content">
-                        <ul id="ul-1">
-                            <li>
-                                <span>Copyright 2023 MN Techgroup India</span>
-                            </li>
-                        </ul>
-                        <ul id="ul-2">
-                            <li><span>Privacy Policy</span></li>
-                            <li><span>Terms of Use</span></li>
-                        </ul>
-                    </div>
+        </div>
+        <div id="footer-box">
+            <div id="footer">
+                <div id="footer-content">
+                    <ul id="ul-1">
+                        <li>
+                            <span>Copyright 2023 MN Techgroup India</span>
+                        </li>
+                    </ul>
+                    <ul id="ul-2">
+                        <li>
+                            <span>Privacy Policy</span>
+                        </li>
+                        <li><span>Terms of Use</span></li>
+
+                    </ul>
+
                 </div>
             </div>
         </div>
+
     </div>
+
+
+    </div>
+
 </body>
 
 </html>
