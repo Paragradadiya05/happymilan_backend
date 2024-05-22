c9fe92c27ccc9de0d15eb91abb2485db9845f9df
parag-radadiya <parag.180410107091@gmail.com>
2024-05-22 13:21:55 +0530

    update firebase service

--- services/notification.service.js
+++ services/notification.service.js
@@ -1,72 +1,88 @@
 import admin from 'firebase-admin';
 
-// Initialize the Firebase Admin SDK
+// TODO: uncomment this line and create FirebaseJson
 const serviceAccount = require('../config/firebase.json');
 
-const app = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-const messaging = app.messaging();
-
 /**
- * Send a notification to a device
- * @param {string|string[]} fcmToken - The FCM token(s) to send the notification to
- * @param {Object} messageData - The message payload to send
+ * intializing the firebase messaging service (push notification)
+ */
+const messaging = admin
+  .initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  })
+  .messaging();
+/* eslint-disable */
+export const verifyFCMToken = async (fcmToken) => {
+  try {
+    const isValid = await messaging.send(
+        {
+          token: fcmToken,
+        },
+        false
+    );
+    return isValid;
+  } catch (er) {}
+};
+
+/* eslint-enable */
+/**
+ * Send an Notification
+ * @param {string || string[]} deviceToken
+ * @param {Object} message
+ * @param {string} options
  * @returns {Promise}
  */
-// eslint-disable-next-line import/prefer-default-export
+// export const sendNotification = async (deviceToken, message) => {
+//   try {
+//     console.log('===sendNotification deviceToken ===>', deviceToken);
+//     console.log('=== sendNotification message ===>', message);
+//     const result = await messaging.sendToDevice(deviceToken, message, {
+//       priority: 'high',
+//       timeToLive: 0,
+//       ttl: 0,
+//     });
+//     console.log('=== sendNotification result ===>', result);
+//     return result;
+//   } catch (e) {
+//     console.log('=== sendNotification error ===>', e);
+//   }
+// };
+
 export const sendNotification = async (fcmToken, messageData) => {
   try {
     const message = {
       notification: {
         ...(messageData.data._id && { _id: messageData.data._id }),
-        ...(messageData.data.userId && { userId: messageData.data.userId }),
-        ...(messageData.data.body && { body: messageData.data.body }),
-        ...(messageData.data.title && { title: messageData.data.title }),
+        ...(messageData.data.userId && { _id: messageData.data.userId }),
+        ...(messageData.data.body && { _id: messageData.data._id }),
+        ...(messageData.data.title && { _id: messageData.data.body }),
       },
     };
-
-    const response = await messaging.sendToDevice(
-      fcmToken,
-      {
-        notification: {
-          title: messageData.data.title,
-          body: messageData.data.body,
-          ...(messageData.data.body && { jsonData: JSON.stringify(message) }),
+    messaging
+      .sendToDevice(
+        fcmToken,
+        {
+          notification: {
+            title: messageData.data.body,
+            ...(messageData.data.body && { jsonData: JSON.stringify(message) }),
+          },
         },
-      },
-      {
-        priority: 'high',
-        timeToLive: 0,
-      }
-    );
-
-    // Handle the response to check for invalid tokens
-    const tokensToRemove = [];
-    response.results.forEach((result, index) => {
-      const { error } = result;
-      if (error) {
-        console.error('Failure sending notification to', fcmToken[index], error);
-        // Check for specific error codes to determine if the token should be removed
-        if (
-          error.code === 'messaging/invalid-registration-token' ||
-          error.code === 'messaging/registration-token-not-registered'
-        ) {
-          tokensToRemove.push(fcmToken[index]);
+        {
+          priority: 'high',
+          timeToLive: 0,
+          ttl: 0,
+          // dryRun: true,
         }
-      }
-    });
-
-    if (tokensToRemove.length > 0) {
-      console.log('Removing invalid tokens:', tokensToRemove);
-      // Implement your logic to remove invalid tokens from your database
-      // removeInvalidTokensFromDatabase(tokensToRemove);
-    }
-
-    return response;
-  } catch (error) {
-    console.error('Error sending notification:', error);
-    throw error;
+      )
+      .then((res) => {
+        if (!res.successCount) {
+          console.log('error in send notification ===>', res.results);
+        }
+      })
+      .catch((e) => {
+        console.log('=== error in send notification outside fun ===>', e);
+      });
+  } catch (er) {
+    console.log('=== error in send notification outside fun catch ===>', er);
   }
 };
