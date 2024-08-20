7501fe2458f60952c5126b8d40286a69ad196728
parag-radadiya <parag.180410107091@gmail.com>
2024-08-20 14:05:39 +0530

    fix upload xlsx api

--- services/uploadxlsx.service.js
+++ services/uploadxlsx.service.js
@@ -1,5 +1,5 @@
-import { User } from 'models';
-
+import { User, Role } from 'models';
+// Ensure Role model is correctly imported
 const xlsx = require('xlsx');
 
 export async function uploadData(file) {
@@ -16,27 +16,46 @@ export async function uploadData(file) {
 
   const emailSet = new Set();
   const numberSet = new Set();
-  //
-  jsonData.forEach((userData) => {
+
+  // Fetch the default role (e.g., "user") from the Role collection
+  const defaultRole = await Role.findOne({ role: 'user' });
+  if (!defaultRole) {
+    throw new Error('Default role "user" not found in the database');
+  }
+
+  // First, check for duplicate emails and mobile numbers within the uploaded data
+  // eslint-disable-next-line no-restricted-syntax
+  for (const userData of jsonData) {
     if (emailSet.has(userData.email)) {
-      throw new Error(`Duplicate email found: ${userData.email}`);
+      throw new Error(`Duplicate email found in the uploaded file: ${userData.email}`);
     }
     if (!userData.email || userData.email.trim() === '') {
-      throw new Error('email wes not added in data');
+      throw new Error('Email was not added in data');
     }
     emailSet.add(userData.email);
-  });
-  //
-  jsonData.forEach((userData) => {
+
     if (numberSet.has(userData.mobileNumber)) {
-      throw new Error(`Duplicate mobileNumber found: ${userData.mobileNumber}`);
+      throw new Error(`Duplicate mobile number found in the uploaded file: ${userData.mobileNumber}`);
     }
     numberSet.add(userData.mobileNumber);
+  }
+
+  // Now, check if any of these emails or mobile numbers already exist in the database
+  const existingUsers = await User.find({
+    $or: [{ email: { $in: Array.from(emailSet) } }],
   });
 
+  if (existingUsers.length > 0) {
+    const existingEmails = existingUsers.map((user) => user.email).join(', ');
+
+    throw new Error(`Users with the following emails  already exist: Emails: ${existingEmails}`);
+  }
+
+  // Set default role as the ObjectId from the Role collection if it's not already provided
   const data = await User.create(
     jsonData.map((userData) =>
       Object.assign(userData, {
+        role: userData.role ? userData.role : defaultRole._id, // Use the ObjectId of the default role
         ...(userData.dateOfBirth && {
           dateOfBirth: new Date((userData.dateOfBirth - 25569) * 86400 * 1000),
         }),
@@ -49,8 +68,10 @@ export async function uploadData(file) {
       })
     )
   );
+
   return data;
 }
+
 export async function getuploaddata(filter, options = {}) {
   const user = await User.find(filter, options.projection, options);
   return user;
