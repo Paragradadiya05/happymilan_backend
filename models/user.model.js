649e83ac39054c26a62d9f66f58b349e0b3ef330
parag-radadiya <parag.180410107091@gmail.com>
2024-09-09 12:36:53 +0530

    birthTime type change

--- models/user.model.js
+++ models/user.model.js
@@ -211,7 +211,8 @@ const UserSchema = new mongoose.Schema(
       type: String,
     },
     birthTime: {
-      type: Date,
+      type: String,
+      match: /^(0?[1-9]|1[0-2]):([0-5]\d)\s?(AM|PM)$/i, // Matches 12-hour format with AM/PM
     },
     religion: {
       type: String,
