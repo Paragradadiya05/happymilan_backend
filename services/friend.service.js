d803b0f2b4e06c889d15aa4449b331616dbac847
parag-radadiya <parag.180410107091@gmail.com>
2024-10-01 17:18:08 +0530

    add aggrigetion in getRequests

--- services/friend.service.js
+++ services/friend.service.js
@@ -1,6 +1,6 @@
 import ApiError from 'utils/ApiError';
 import httpStatus from 'http-status';
-import { Friend, Notification, Partner, Shortlist, User } from 'models';
+import { Friend, Notification, Partner, User } from 'models';
 import { EnumOfNotification, EnumStatusOfFriend } from '../models/enum.model';
 import { sendNotification } from './notification.service';
 
@@ -415,22 +415,16 @@ export async function getFriendv2(filter, options = {}, userId) {
     throw new Error('User Partner Preferences not found');
   }
 
-  // Fetch shortlist data for the user
-  const shortlists = await Shortlist.find({ userId }).select('shortlistId'); // Get only the shortlist IDs
-
-  // Iterate over each friend to calculate the match score and add shortlist data
+  // Iterate over each friend to calculate the match score
   const friendsWithMatchScore = await Promise.all(
     friends.map(async (friend) => {
       const friendId = friend.friend ? friend.friend._id : friend.user._id;
 
       const matchScore = await calculateMatchScore(friendId, userPartnerPreferences);
 
-      const isShortlisted = shortlists.some((shortlist) => shortlist.shortlistId.equals(friendId));
-
       return {
         ...friend.toObject(),
         matchScore,
-        isShortlisted, // Indicate if the friend is shortlisted
       };
     })
   );
