b9f4bacc67363849f4b60d9e8beabffa31444475
parag-radadiya <parag.180410107091@gmail.com>
2024-10-10 15:47:21 +0530

    populate in notification

--- services/notificationmsg.service.js
+++ services/notificationmsg.service.js
@@ -2,9 +2,9 @@ import httpStatus from 'http-status';
 import { Notification, User } from '../models';
 import ApiError from '../utils/ApiError';
 
-export async function createNotification(body = {}) {
+export async function createNotification(appUsesType, body = {}) {
   if (body.userId) {
-    const userId = await User.findOne({ _id: body.userId });
+    const userId = await User.findOne({ _id: body.userId, appUsesType });
     if (!userId) {
       throw new ApiError(httpStatus.BAD_REQUEST, 'field userId is not valid');
     }
@@ -14,7 +14,10 @@ export async function createNotification(body = {}) {
 }
 
 export async function getNotification(filter, options = {}) {
-  const notification = await Notification.find(filter, options.projection, options);
+  const notification = await Notification.find(filter, options.projection, options).populate({
+    path: 'userId',
+    select: 'name profilePic',
+  });
   return notification;
 }
 export async function getOne(query, options = {}) {
