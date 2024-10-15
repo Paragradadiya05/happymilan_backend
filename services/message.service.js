482bd27751de2b039b37c0c357cf777314135849
parag-radadiya <parag.180410107091@gmail.com>
2024-10-15 12:52:39 +0530

    set notifications send  message

--- services/message.service.js
+++ services/message.service.js
@@ -1,10 +1,48 @@
-import { Message } from 'models';
+import { Message, User, Notification } from 'models';
+import httpStatus from 'http-status';
+import ApiError from '../utils/ApiError';
+import { sendNotification } from './notification.service';
 
 export async function createMessage(body = {}) {
   const message = await Message.create(body);
+
+  const fromUserId = body.from.toString();
+  const toUserId = body.to.toString();
+
+  // Get sender and recipient details
+  const fromUser = await User.findById(fromUserId);
+  const toUser = await User.findById(toUserId);
+
+  if (!toUser) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Recipient does not exist');
+  }
+
+  if (!toUser.isUserActive) {
+    // Create notification for the recipient (to) if they are offline
+    const notificationForRecipient = await Notification.create({
+      userId: toUserId,
+      otherUserId: fromUserId,
+      body: `Message received from ${fromUser.name}`,
+    });
+
+    // Send notification to recipient (to) if they are offline
+    if (toUser.deviceTokens.length) {
+      await toUser.deviceTokens.map(async (fcmToken) => {
+        await sendNotification(fcmToken.deviceToken, {
+          data: {
+            _id: notificationForRecipient._id.toString(),
+            userId: toUserId,
+            otherUserId: fromUserId,
+            body: `Message received from ${fromUser.name}`,
+            title: 'Message Received',
+          },
+        });
+      });
+    }
+  }
+
   return message;
 }
-
 export async function getMessageList(filter, options = {}) {
   const message = await Message.find(filter, options.projection, options).sort({ createdAt: -1 }).limit(5);
   return message;
