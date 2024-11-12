d93da66f4b85e6bc665fcb65a6b0281c1cfabdeb
parag-radadiya <parag.180410107091@gmail.com>
2024-11-12 16:32:45 +0530

    fix req sent

--- services/friend.service.js
+++ services/friend.service.js
@@ -209,7 +209,7 @@ export async function getFriendList(filter, options = {}) {
 
   // Return paginated results with metadata
   return {
-    docs: friendsWithMatchData,
+    results: friendsWithMatchData,
     totalDocs,
     limit,
     page,
