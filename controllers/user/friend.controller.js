c821e8560fdecd28ba534fb99d976135d33bbc46
parag-radadiya <parag.180410107091@gmail.com>
2025-01-06 17:23:22 +0530

    fix reject api friend

--- controllers/user/friend.controller.js
+++ controllers/user/friend.controller.js
@@ -162,11 +162,6 @@ export const getMyFrdRequestsMobile = catchAsync(async (req, res) => {
   return res.status(httpStatus.OK).send({ results: getuser });
 });
 export const getRejectedFrdRequests = catchAsync(async (req, res) => {
-  const userId = req.user._id;
-  const filter = {
-    user: userId,
-    status: EnumStatusOfFriend.REJECTED,
-  };
   const { query } = req;
   const sortingObj = pick(query, ['sort', 'order']);
   const sortObj = {
@@ -177,9 +172,34 @@ export const getRejectedFrdRequests = catchAsync(async (req, res) => {
     ...pick(query, ['limit', 'page']),
     lean: true,
   };
-  const user = await friendService.getFriendMobile(filter, options, userId);
+  const userId = req.user._id;
+  const filter = {
+    status: EnumStatusOfFriend.REJECTED,
+    $or: [{ friend: userId }, { user: userId }],
+  };
+  const getuser = await friendService.getFriendMobile(filter, options, userId);
+  getuser.results = getuser.results.map((frdData) => {
+    let friendList;
+    let userList;
+    if (frdData.friend._id.toString() === userId.toString()) {
+      friendList = frdData.user;
+      userList = frdData.friend;
+    } else {
+      friendList = frdData.friend;
+      userList = frdData.user;
+    }
 
-  return res.status(httpStatus.OK).send({ results: user });
+    const { friend, user, ...restFrdData } = frdData;
+    if (friendList.shortlistData === undefined || friendList.shortlistData.length === 0) {
+      delete friendList.shortlistData;
+    }
+    return {
+      ...restFrdData,
+      friendList,
+      userList,
+    };
+  });
+  return res.status(httpStatus.OK).send({ results: getuser });
 });
 
 export const getRequestedFriend = catchAsync(async (req, res) => {
