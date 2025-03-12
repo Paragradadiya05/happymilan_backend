bcc3d6800878c986dd88f57a6d0f6e49b70ecdca
parag-radadiya <parag.180410107091@gmail.com>
2025-03-12 15:37:16 +0530

    fix validation for profilePhotoPrivacy

--- validations/user/user.validation.js
+++ validations/user/user.validation.js
@@ -196,7 +196,7 @@ export const updateUserPrivacy = {
   body: Joi.object().keys({
     privacySetting: Joi.string().valid('publicProfile', 'privateProfile', 'premiumProfile'),
     privacySettingCustom: Joi.object().keys({
-      profilePhotoPrivacy: Joi.string().valid('public', 'private', 'no-photo'),
+      profilePhotoPrivacy: Joi.string().valid('public', 'private', 'no-photo', 'premium'),
       publicProfile: Joi.array().items(Joi.string()),
       privateProfile: Joi.array().items(Joi.string()),
       premiumProfile: Joi.array().items(Joi.string()),
