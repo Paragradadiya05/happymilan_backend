ba2b0aab8b29d9457c0ba06d0ea6ffa5888effd5
parag-radadiya <parag.180410107091@gmail.com>
2025-04-22 17:03:32 +0530

    fix profileviewer data

--- services/profileviewer.service.js
+++ services/profileviewer.service.js
@@ -386,7 +386,7 @@ export async function getProfileViewertWithPagination(filter, options = {}) {
         shortlistData: 1,
       },
     },
-    { $sort: { matchPercentage: -1 } }, // Sort by match percentage in descending order
+    { $sort: { createdAt: -1 } }, // Sort by match percentage in descending order
     // todo
 
     // { $skip: (page - 1) * limit }, // Skip documents for pagination
