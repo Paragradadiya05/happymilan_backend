65f824af88b6045c2255c41af7251145276d4d30
parag-radadiya <parag.180410107091@gmail.com>
2025-05-20 13:56:20 +0530

    fix error server

--- routes/common/aws/s3.route.js
+++ routes/common/aws/s3.route.js
@@ -18,7 +18,7 @@ router.post('/uploadstoryimage', auth(), validate(s3Validation.UploadStoryImg), 
 
 router.post('/send-proposal', validate(s3Validation.sendProposal), s3Controller.sendProposal);
 
-router.post('/apply-internship', validate(s3Validation.ApplyInternship), s3Validation.ApplyInternship);
+router.post('/apply-internship', validate(s3Validation.ApplyInternship), s3Controller.ApplyInternship);
 
 router.post('/csr-initiative', validate(s3Validation.csrInitiative), s3Controller.csrInitiative);
 
