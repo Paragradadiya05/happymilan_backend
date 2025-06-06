b8ad4d26dab12b4d24298ea9fe9f9ea4f027f796
parag-radadiya <parag.180410107091@gmail.com>
2025-06-06 16:18:23 +0530

    fix notification

--- controllers/user/notification.controller.js
+++ controllers/user/notification.controller.js
@@ -21,7 +21,7 @@ export const getNotificationById = catchAsync(async (req, res) => {
   const { user } = req;
   const filter = {
     userId: user,
-    title: { $in: ['Request-received', 'request-accepted', 'like'] },
+    title: { $in: ['Sent you a request', 'request-accepted', 'like'] },
   };
   const notification = await notificationservice.getNotification(filter, options);
   return res.status(httpStatus.OK).send({ results: notification });
