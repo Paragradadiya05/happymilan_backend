0187fb3af43fb9d4502eeed076fc31c87d97a834
parag-radadiya <parag.180410107091@gmail.com>
2025-06-20 12:01:27 +0530

    fix dating api

--- services/user.service.js
+++ services/user.service.js
@@ -1634,12 +1634,6 @@ export async function getDatingPartnerList(filter, options = {}) {
       },
     },
     {
-      $unwind: {
-        path: '$userLikeDetails',
-        preserveNullAndEmptyArrays: true, // Include users with no matching friends
-      },
-    },
-    {
       $lookup: {
         from: 'shortlists',
         let: { currentUserIdForShortList: '$_id' },
@@ -1653,12 +1647,6 @@ export async function getDatingPartnerList(filter, options = {}) {
           },
         ],
         as: 'userShortListDetails',
-      },
-    },
-    {
-      $unwind: {
-        path: '$userShortListDetails',
-        preserveNullAndEmptyArrays: true,
       },
     },
     {
@@ -1684,12 +1672,6 @@ export async function getDatingPartnerList(filter, options = {}) {
           },
         ],
         as: 'friendsDetails',
-      },
-    },
-    {
-      $unwind: {
-        path: '$friendsDetails',
-        preserveNullAndEmptyArrays: true, // Include users with no matching friends
       },
     },
     {
