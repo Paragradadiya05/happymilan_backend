699fe156aa75d896cf6ce1169961510785d2e7d2
parag-radadiya <parag.180410107091@gmail.com>
2025-07-28 14:02:19 +0530

    fix login

--- services/auth.service.js
+++ services/auth.service.js
@@ -444,7 +444,7 @@ export const loginUserWithEmailOrMobileAndPassword = async (email, mobileNumber,
   if (!user) {
     throw new ApiError(httpStatus.BAD_REQUEST, 'Incorrect email or password');
   }
-  if (user.profileHideAndDelete.isProfileDelete) {
+  if (user.profileHideAndDelete && user.profileHideAndDelete.isProfileDelete === true) {
     throw new ApiError(httpStatus.BAD_REQUEST, 'Your account has been deleted.');
   }
   const isValid = await bcrypt.compare(password, user.password);
