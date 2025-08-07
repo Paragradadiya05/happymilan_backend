4729f017535728a655fccde53ec364893c0b8504
parag-radadiya <parag.180410107091@gmail.com>
2025-08-07 11:57:31 +0530

    set for plan inactive

--- controllers/user/userPlan.controller.js
+++ controllers/user/userPlan.controller.js
@@ -1,15 +1,28 @@
 import httpStatus from 'http-status';
 import { catchAsync } from '../../utils/catchAsync';
 import { userPlanService } from '../../services';
+import { UserPlan } from '../../models';
 
 export const getUserPlanId = catchAsync(async (req, res) => {
   const userId = req.user._id;
-  const filter = {
-    userId,
-  };
+
+  const filter = { userId };
   const options = {};
-  const user = await userPlanService.getOne(filter, options);
-  return res.status(httpStatus.OK).send({ results: user });
+  const userPlan = await userPlanService.getOne(filter, options);
+
+  if (!userPlan) {
+    return res.status(httpStatus.NOT_FOUND).send({ message: 'User plan not found' });
+  }
+
+  const currentDate = new Date();
+
+  // If the end date has passed and status is still active, mark it as inactive
+  if (userPlan.endDate && userPlan.endDate < currentDate && userPlan.status !== 'inactive') {
+    userPlan.status = 'inactive';
+    await UserPlan.save();
+  }
+
+  return res.status(httpStatus.OK).send({ results: userPlan });
 });
 
 export const list = catchAsync(async (req, res) => {
