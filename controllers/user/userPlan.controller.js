62e24f8b5aeded09f39882448367962db16bd191
parag-radadiya <parag.180410107091@gmail.com>
2025-09-01 15:17:36 +0530

    fix pdf error

--- controllers/user/userPlan.controller.js
+++ controllers/user/userPlan.controller.js
@@ -118,7 +118,11 @@ export const downloadUserPlanReceipt = catchAsync(async (req, res) => {
   `;
 
   // Launch Puppeteer and generate PDF
-  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+  const browser = await puppeteer.launch({
+    executablePath: '/usr/bin/google-chrome',
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    headless: true,
+  });
   const page = await browser.newPage();
   await page.setContent(htmlContent, { waitUntil: 'networkidle0' });
   const pdfBuffer = await page.pdf({ format: 'A4', printBackground: true });
