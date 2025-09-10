95f382f2db8fadd572395716ec059233e009aeb8
parag-radadiya <parag.180410107091@gmail.com>
2025-09-10 10:50:57 +0530

    fix gender api

--- services/user.service.js
+++ services/user.service.js
@@ -1062,37 +1062,22 @@ export async function getGenderListV2(filter, options = {}) {
                       value: {
                         $cond: [
                           {
-                            $and: [
-                              { $isArray: '$userPartnerDetails.diet' },
-                              { $eq: [{ $type: { $arrayElemAt: ['$userPartnerDetails.diet', 0] } }, 'array'] },
-                            ],
+                            $and: [{ $isArray: '$diet' }, { $eq: [{ $type: { $arrayElemAt: ['$diet', 0] } }, 'array'] }],
                           },
-                          { $arrayElemAt: ['$userPartnerDetails.diet', 0] }, // unwrap [["vegetarian"]] → ["vegetarian"]
-                          {
-                            $cond: [
-                              { $isArray: '$userPartnerDetails.diet' },
-                              '$userPartnerDetails.diet', // already ["vegetarian"]
-                              [],
-                            ],
-                          },
+                          { $arrayElemAt: ['$diet', 0] }, // unwrap [["reading","cooking"]] → ["reading","cooking"]
+                          { $ifNull: ['$diet', []] },
                         ],
                       },
                       expected: {
                         $cond: [
                           {
                             $and: [
-                              { $isArray: userPartnerPreferences.diet },
-                              { $eq: [{ $type: { $arrayElemAt: [userPartnerPreferences.diet, 0] } }, 'array'] },
+                              { $isArray: '$userPartnerDetails.diet' },
+                              { $eq: [{ $type: { $arrayElemAt: ['$userPartnerDetails.diet', 0] } }, 'array'] },
                             ],
                           },
-                          { $arrayElemAt: [userPartnerPreferences.diet, 0] }, // unwrap [["vegetarian"]] → ["vegetarian"]
-                          {
-                            $cond: [
-                              { $isArray: userPartnerPreferences.diet },
-                              userPartnerPreferences.diet, // already ["vegetarian"]
-                              [],
-                            ],
-                          },
+                          { $arrayElemAt: ['$userPartnerDetails.diet', 0] },
+                          { $ifNull: ['$userPartnerDetails.diet', []] },
                         ],
                       },
                     },
