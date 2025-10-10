8c8d1372b174a46041be12f3d95c00e058811ae7
parag-radadiya <parag.180410107091@gmail.com>
2025-10-10 19:10:59 +0530

    update payment for mobile

--- validations/user/order.validation.js
+++ validations/user/order.validation.js
@@ -12,7 +12,7 @@ export const orderComplete = {
   query: Joi.object().keys({
     paymentHistoryToken: Joi.string().required(),
     authToken: Joi.string().required(),
-    client_type: Joi.string().required().valid('mobile', 'web'),
+    client_type: Joi.string().valid('mobile', 'web'),
   }),
   body: Joi.object()
     .keys({
