21d7e8d0f7b56aadb9912bdff41dd431169b7cd2
parag-radadiya <parag.180410107091@gmail.com>
2025-12-30 16:33:52 +0530

    fix port

--- config/config.js
+++ config/config.js
@@ -7,7 +7,7 @@ dotenv.config({ path: path.resolve(process.cwd(), '.env') });
 const envVarsSchema = Joi.object()
   .keys({
     NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
-    PORT: Joi.number().default(3050),
+    PORT: Joi.number().required(),
     LOG_LEVEL: Joi.string().required().description('log level'),
     MONGODB_URL: Joi.string().required().description('Mongo DB url'),
     JWT_SECRET: Joi.string().required().description('JWT secret key'),
